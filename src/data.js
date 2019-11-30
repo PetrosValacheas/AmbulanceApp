1f5e9bb33042bebc1e7f994ba7a7148fb728d249
PetrosVal <petrosva12gmail.com>
2019-11-30 20:36:27 +0200

    Add-Images-for-Home-page

--- src/data.js
+++ src/data.js
@@ -6,8 +6,8 @@ export const homeArticles = [
     title: 'Stroke',
     content:'1.Let the person lie confortably on one side with their hand slightly raised and supported, in case they vomit 2.If the person is having difficulty breathing, loosen any constrictive clothing 3. If the person is not breathing perform  cpr 4. Keep them warm , eg cover them with a blanket 5. Help the injured person lie down. if possible, place the personon a rug or blanketto prevent loss of body heat.Calmly reassure the injured person. ',
     img: {
-      name: 'stethoscope.jpg',
-      alt: 'stethoscope'
+      name: 'stroke.jpg',
+      alt: 'stroke'
     }
   },
   {
@@ -15,8 +15,8 @@ export const homeArticles = [
     content:
       '1. These wounds should not be immersed , as cool water can intensify the circulatory shock taht accompaniew third-degree burns 2. Cover the injuries with bulky ,sterile dressings or with freshly laundered bed linens 3. Clothing stuck to the wound should not be rremoved , nor should any oitements , salves, sprays ,etc. be applied 4. Burned feet and legs should be elevated and burned hands should be raised above the level of the heart 5.The vicrims breathing must be closely watched; artificial respiration should be givenif breathing stops.',
     img: {
-      name: 'medic.jpg',
-      alt: 'medic'
+      name: 'heat.jpg',
+      alt: 'heat'
     }
   },
   {
@@ -33,8 +33,8 @@ export const homeArticles = [
     content:
       '1. See if they have ana epinephrine (adrenaline) auto-injector (EpiPen) and help them , if nedded 2.Try to keep the person calm 3.Help the person lie on their back 4.Raise their feet about 12 inches and cover them with a blanket 5.Turn them on their side if they are vomitting or bleeding 6.Make sure their clothing is loose so sthey can breathe.',
     img: {
-      name: 'dna.jpg',
-      alt: 'dna'
+      name: 'anaphylaxis.jpg',
+      alt: 'anaphylaxis'
     }
   },
   {
@@ -42,8 +42,8 @@ export const homeArticles = [
     content:
       '1. Avoid solid foods until vommiting ends. Then eat light , bland foods , such as saltine crackers, bananas , rice or bread  2. Sipping liquids may help avoid vommiting 3. Do not eat fried , geasy , spicy or sweet foods 4. Avoid caffeine , alcohol and nicotine 5. Do not take anti-nausea or anti-diarthea medication without asking your doctor.They have side effects and may make some kinds of diarrhea 6. Drink clear fluids , starting wit small sips and gradually drink more 7. If vomiting and diarrhea lasts more than 24 hours , drink oral rehydration solution 8. Food poisoning may be from seafood , or wild mushrooms.Sign of severe dehydration, such as dry mouth , decreased urination ,dizziness , fatigue, or increased heart rate or breathing rate.Severe belly pain.Fever.Bloody diarrhea or dark stools.Prolonged or bloody vomiting. ',
     img: {
-      name: 'dna.jpg',
-      alt: 'dna'
+      name: 'poison.jpg',
+      alt: 'poison'
     }
   },
   {
@@ -51,17 +51,17 @@ export const homeArticles = [
     content:
       '1. Chew and swallow aspirin , if not allergic 2. Do not get the person to cough repeatedly 3. If the person is unconscious ,begin CPR 4. Follow AED instructions , if the person is unconsciously and AED is available. ',
     img: {
-      name: 'dna.jpg',
-      alt: 'dna'
+      name: 'heart.jpg',
+      alt: 'heart'
     }
   },
   {
-    title: "Heart Stroke/Exhaustion",
+    title: "Heat Stroke/Exhaustion",
     content:
       '1. Move person to a cool or air-conditioned environment 2. Remove any unnecessary clothing 3.Fan air over the patient while wetting his/her skin with water from a sponge/hose 4. Rehydrate with water or sports drinks.Avoid sugary or alcoholic beverages 5. Apply ice packs to the patients armpits, groin , neck and back 6.If possible , immerse patient in a shower or tub of cool water.',
     img: {
-      name: 'dna.jpg',
-      alt: 'dna'
+      name: 'heat.jpg',
+      alt: 'heat'
     }
   },
   {
@@ -69,8 +69,8 @@ export const homeArticles = [
     content:
       '1. Stop any bleeding. if they are bleding, elevate and apply pressure to the wound using a sterile bandage, a clean cloth , or a clean piece of clothing 2. Immobilize the injured area. If you suspect they have broken a bone in their neck or back,help them stay as still as possible.If you suspect they have broken a abone in one of their limbs , immobilize the area using a splint or sling 3.Apply cold to the area; Wrap an ice pack or bag of ice cubes in a piece of cloth and apply it to the injured area for up to 10 minutes at a time 4. Treat them for shock; Help them get into a comfortable position , encourage them to rest and reassure them. Cover them with a blanket or clothing to keep them warm',
     img: {
-      name: 'dna.jpg',
-      alt: 'dna'
+      name: 'bone.jpg',
+      alt: 'bone'
     }
   },
 ];
