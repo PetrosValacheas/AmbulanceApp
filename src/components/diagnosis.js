286361fee110cc5150df13dda0eb8b79ec4f6156
PetrosVal <petrosva12gmail.com>
2019-11-30 14:20:10 +0200

    Add-Symptom-Selection-Functionality

--- src/components/diagnosis.js
+++ src/components/diagnosis.js
@@ -5,17 +5,56 @@ import React, { Component } from 'react';
   	constructor(props) {
    	 super(props);
          this.state = {
-      	 contacts: []
+      	 contacts: [],
+      	 selectedIds: []
     	};
+
+    	this.onCheckChange = this.onCheckChange.bind(this)
 	}
 
  	componentDidMount() {
-        fetch('https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InBldHJvc3ZhMTJAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2MTEwIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDE5LTExLTIzIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE1NzUxMTA4ODYsIm5iZiI6MTU3NTEwMzY4Nn0.zP5NTHgRfPd9jOT6TbyArvkonIDCfRtArldjF1KzcQM&language=en-gb&format=json')
+        fetch('https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InBldHJvc3ZhMTJAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2MTEwIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDE5LTExLTIzIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE1NzUxMTg4NTUsIm5iZiI6MTU3NTExMTY1NX0.Sat0WCTsfIBvVqnTyOzeTGs-lmJ7L3esapp4rN-s3AA&language=en-gb&format=json')
         .then(res => res.json())
         .then((data) => {
         	this.setState({ contacts: data })
         })
         .catch(console.log)
+    }
+
+    onCheckChange(e){
+
+    	let id = e.currentTarget.getAttribute('id')
+
+    	const {selectedIds} = this.state.selectedIds;
+
+    	console.log(this.state.selectedIds);
+   
+
+    	if(e.target.checked===true){
+    		console.log("ID: "+id);
+
+    		this.setState(prevState => ({
+   					selectedIds: [...prevState.selectedIds,id]
+			}))
+    	
+
+    	}else{
+
+    		let array = [...this.state.selectedIds];
+    		let index = array.indexOf(id);
+    		console.log("iNDEX:"+index);
+    		if(index!=-1){
+
+    			array.splice(index,1);
+    			this.setState({selectedIds: array})
+    		}
+	
+    	}
+
+    	this.setState({
+    		[e.target.name]: e.target.checked
+    	})
+
     }
     
   	render(){
@@ -32,6 +71,9 @@ import React, { Component } from 'react';
           	 <div className="card">
               <div className="card-body">
                 <h2 className="card-title">{contact.Name}</h2>
+
+                <input type="checkbox"  className="symptomsCheckbox" id={contact.ID} onChange={this.onCheckChange}/>
+
                
                </div>
              </div>
