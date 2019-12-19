855feca8827d6b4402ad7f403ec04f355284befd
PetrosVal <petrosva12gmail.com>
2019-12-19 09:45:55 +0200

    Add-firebase-Config

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var DB_CONFIG= {
+    apiKey: "AIzaSyA6OABLVeGtCRGJZkeFOCCF2GgB0o7GlBU",
+    authDomain: "ambulanceconsulting.firebaseapp.com",
+    databaseURL: "https://ambulanceconsulting.firebaseio.com",
+    projectId: "ambulanceconsulting",
+    storageBucket: "ambulanceconsulting.appspot.com",
+    messagingSenderId: "1091294534215",
+    appId: "1:1091294534215:web:8b12a44fe0a406dc516f62"
+  };
+
+
