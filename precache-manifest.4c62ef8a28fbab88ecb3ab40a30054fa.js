4763d308481504edf292b15633fe13543d01d2da
root <root@DESKTOP-2M2HDD8.localdomain>
2020-01-21 21:59:40 +0200

    Updates

---
+++ precache-manifest.4c62ef8a28fbab88ecb3ab40a30054fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2230e53e2ef6d4ebfcf1",
+    "url": "/AmbulanceApp/static/js/runtime~main.92451580.js"
+  },
+  {
+    "revision": "b28751e1a9da6d327dbc",
+    "url": "/AmbulanceApp/static/js/main.49f6eb6b.chunk.js"
+  },
+  {
+    "revision": "bd109b662fa3dea387cd",
+    "url": "/AmbulanceApp/static/js/2.837a65cc.chunk.js"
+  },
+  {
+    "revision": "b28751e1a9da6d327dbc",
+    "url": "/AmbulanceApp/static/css/main.475973f1.chunk.css"
+  },
+  {
+    "revision": "9350463938647324c2e2c24d430eb96d",
+    "url": "/AmbulanceApp/index.html"
+  }
+];
